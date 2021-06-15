29253061b1b0f61a8217396e1371d38d7ecfbcf6
Josué Getsemani Sandoval Villarreal <getsemanilol29@gmail.com>
2021-06-15 12:08:25 -0700

    logica 'terminada'

--- js/index.js
+++ js/index.js
@@ -17,13 +17,56 @@ addEventListener('DOMContentLoaded', () => {
 
 /* funciones */
 const rockEvent = () => {
-  console.log('Click Rock');
+  return playingVsPc('rock');
 };
 
 const paperEvent = () => {
-  console.log('Click Paper');
+  return playingVsPc('paper');
 };
 
 const scissorEvent = () => {
-  console.log('Click Scissor');
+  return playingVsPc('scissor');
+};
+
+const playingVsPc = (eventPlayer = '') => {
+  const PC = Math.floor(Math.random() * 3) + 1;
+
+  console.log(PC);
+  switch (eventPlayer) {
+    case 'rock':
+      let rock = 2;
+      if (rock === PC) {
+        return console.log(`Son iguales ${rock === PC}`);
+      } else if (rock >= PC) {
+        return console.log(`Jugador es mayor que PC ${rock >= PC}`);
+      } else if (rock <= PC) {
+        return console.log(`PC es mayor que Jugador ${PC >= rock}`);
+      }
+      break;
+
+    case 'paper':
+      let paper = 2;
+      if (paper === PC) {
+        return console.log(`Son iguales ${paper === PC}`);
+      } else if (paper >= PC) {
+        return console.log(`Jugador es mayor que PC ${paper >= PC}`);
+      } else if (paper <= PC) {
+        return console.log(`PC es mayor que Jugador ${PC >= paper}`);
+      }
+      break;
+
+    case 'scissor':
+      let scissor = 2;
+      if (scissor === PC) {
+        return console.log(`Son iguales ${scissor === PC}`);
+      } else if (scissor >= PC) {
+        return console.log(`Jugador es mayor que PC ${scissor >= PC}`);
+      } else if (scissor <= PC) {
+        return console.log(`PC es mayor que Jugador ${PC >= scissor}`);
+      }
+      break;
+    default:
+      console.log('error no se selecciono ninguno');
+      break;
+  }
 };
